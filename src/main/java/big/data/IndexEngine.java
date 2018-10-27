2a6ff2db875d0d92430d8d9fae95b83054818f0b
BorisAnimal <iguru98@yandex.ru>
2018-10-27 15:29:09 -0400

    index works

--- src/main/java/big/data/IndexEngine.java
+++ src/main/java/big/data/IndexEngine.java
@@ -1,71 +1,85 @@
 package big.data;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
+import java.util.*;
+
 import org.apache.commons.io.FileUtils;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.MapWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-import org.apache.hadoop.util.Tool;
-import org.apache.hadoop.util.ToolRunner;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.util.StringUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.*;
+public class IndexEngine {
+    public static class TokenizerMapper extends Mapper<Object, Text, IntWritable, IntWritable> {
+        private IntWritable whash = new IntWritable();
+        private boolean caseSensitive = true;
+        private Set<String> patternsToSkip = new HashSet<String>();
 
+        private Configuration conf;
+        private BufferedReader fis;
 
-public class IndexEngine extends Configured implements Tool {
-
-    public static class MapperIDF extends Mapper<LongWritable, Text, IntWritable, IntWritable> {
-        private static final IntWritable ONE = new IntWritable(1);
-        private final transient IntWritable WORD_HASH = new IntWritable();
-
-        private boolean caseSensitive = false;
-        private Set<String> patternsToSkip = new HashSet<String>(); //TODO
-
-        /**
-         * Counts IDF for words
-         */
         @Override
-        public void map(final LongWritable key, final Text value, final Context context) throws IOException, InterruptedException {
-//            final String line = value.toString();
-//            final StringTokenizer tokenizer = new StringTokenizer(line);
-//            while (tokenizer.hasMoreTokens()) {
-//                word.set(tokenizer.nextToken());
-//                context.write(word, ONE);
-//            }
+        public void setup(Context context) throws IOException {
+            conf = context.getConfiguration();
+//            caseSensitive = conf.getBoolean("wordcount.case.sensitive", true);
+            if (conf.getBoolean("wordcount.skip.patterns", false)) {
+                URI[] patternsURIs = Job.getInstance(conf).getCacheFiles();
+                for (URI patternsURI : patternsURIs) {
+                    Path patternsPath = new Path(patternsURI.getPath());
+                    String patternsFileName = patternsPath.getName().toString();
+                    parseSkipFile(patternsFileName);
+                }
+            }
+        }
 
-            // Preprocessing
+        private void parseSkipFile(String fileName) {
+            try {
+                fis = new BufferedReader(new FileReader(fileName));
+                String pattern = null;
+                while ((pattern = fis.readLine()) != null) {
+                    patternsToSkip.add(pattern);
+                }
+            } catch (IOException ioe) {
+                System.err.println("Caught exception while parsing the cached file '"
+                        + StringUtils.stringifyException(ioe));
+            }
+        }
+
+        @Override
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
             String line = (caseSensitive) ? value.toString() : value.toString().toLowerCase();
-//            for (String pattern : patternsToSkip) {
-//                line = line.replaceAll(pattern, "");
-//            }
+            for (String pattern : patternsToSkip) {
+                line = line.replaceAll(pattern, "");
+            }
 
             String[] tuple = line.split("\\n");
             try {
                 // For each document (JSON)
                 for (String str : tuple) {
                     JSONObject obj = new JSONObject(str);
-
-                    HashSet<Integer> hashes = new HashSet<Integer>();
-                    final StringTokenizer itr = new StringTokenizer(obj.getString("text"));
+                    IntWritable d_id = new IntWritable(obj.getInt("id"));
+                    String text = obj.getString("text");
+                    StringTokenizer itr = new StringTokenizer(text);
                     while (itr.hasMoreTokens()) {
-                        WORD_HASH.set(itr.nextToken().hashCode());
-                    }
-                    for (int i : hashes) {
-                        System.out.println(i + "");
-                        WORD_HASH.set(i);
-                        context.write(WORD_HASH, ONE);
+                        whash.set(itr.nextToken().hashCode());
+                        context.write(d_id, whash);
                     }
                 }
             } catch (JSONException e) {
@@ -74,55 +88,60 @@ public class IndexEngine extends Configured implements Tool {
         }
     }
 
+    public static class IntSumReducer extends Reducer<IntWritable, IntWritable, IntWritable, MapWritable> {
+        private final IntWritable ONE = new IntWritable(1);
 
-    public static class ReducerIDF extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable> {
-
-        @Override
-        public void reduce(final IntWritable key, final Iterable<IntWritable> values, final Context context)
-                throws IOException, InterruptedException {
-            int sum = 0;
-            for (final IntWritable val : values) {
-                sum += val.get();
+        public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            MapWritable counter = new MapWritable();
+            for (IntWritable val : values) {
+                if (counter.containsKey(val)) {
+                    counter.put(new IntWritable(val.get()), new IntWritable(((IntWritable) counter.get(val)).get() + 1));
+                } else {
+                    counter.put(new IntWritable(val.get()), ONE);
+                }
             }
-            context.write(key, new IntWritable(sum));
+            context.write(key, counter);
         }
     }
 
-
-    @Override
-    public int run(final String[] args) throws Exception {
-        final Configuration conf = this.getConf();
-        final Job job = Job.getInstance(conf, "idf_counter");
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        GenericOptionsParser optionParser = new GenericOptionsParser(conf, args);
+        String[] remainingArgs = optionParser.getRemainingArgs();
+        if ((remainingArgs.length != 2) && (remainingArgs.length != 4)) {
+            System.err.println("Usage: <command> <in> <out> [-skip skipPatternFile]");
+            System.exit(2);
+        }
+        Job job = Job.getInstance(conf, "index_engine");
         job.setJarByClass(IndexEngine.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setReducerClass(IntSumReducer.class);
 
-        job.setMapperClass(MapperIDF.class);
-        job.setReducerClass(ReducerIDF.class);
         job.setMapOutputKeyClass(IntWritable.class);
-        job.setMapOutputValueClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
         job.setOutputKeyClass(IntWritable.class);
-        job.setOutputValueClass(Text.class);
+        job.setOutputValueClass(MapWritable.class);
         job.setInputFormatClass(TextInputFormat.class);
-        job.setOutputFormatClass(TextOutputFormat.class);
+        job.setOutputFormatClass(SequenceFileOutputFormat.class);
 
-        FileInputFormat.addInputPath(job, new Path(args[0]));
+
+        List<String> otherArgs = new ArrayList<String>();
+        for (int i = 0; i < remainingArgs.length; ++i) {
+            if ("-skip".equals(remainingArgs[i])) {
+                job.addCacheFile(new Path(remainingArgs[++i]).toUri());
+                job.getConfiguration().setBoolean("index_engine.skip.patterns", true);
+            } else {
+                otherArgs.add(remainingArgs[i]);
+            }
+        }
+        FileInputFormat.addInputPath(job, new Path(otherArgs.get(0)));
         File tmpDir = new File(args[1]);
         boolean exists = tmpDir.exists();
         if (exists) {
             FileUtils.deleteDirectory(tmpDir);
         }
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs.get(1)));
 
-        return job.waitForCompletion(true) ? 0 : 1;
-    }
-
-    public static void main(final String[] args) throws Exception {
-        if (args.length != 2) {
-            System.err.println("Usage: <command> <in> <out>");
-            System.exit(1);
-        }
-
-        final int returnCode = ToolRunner.run(new Configuration(), new IndexEngine(), args);
-        System.exit(returnCode);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
-
--- src/main/java/big/data/SearchEngine.java
+++ src/main/java/big/data/SearchEngine.java
@@ -19,140 +19,132 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.Counter;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.MapFileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 import org.apache.hadoop.util.StringUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class SearchEngine {
-
-    public static class TokenizerMapper extends Mapper<Object, Text, IntWritable, IntWritable> {
-
-        private IntWritable whash = new IntWritable();
-
-        private boolean caseSensitive = true;
-        private Set<String> patternsToSkip = new HashSet<String>();
-
-        private Configuration conf;
-        private BufferedReader fis;
-
-        @Override
-        public void setup(Context context) throws IOException {
-            conf = context.getConfiguration();
-//            caseSensitive = conf.getBoolean("wordcount.case.sensitive", true);
-            if (conf.getBoolean("wordcount.skip.patterns", false)) {
-                URI[] patternsURIs = Job.getInstance(conf).getCacheFiles();
-                for (URI patternsURI : patternsURIs) {
-                    Path patternsPath = new Path(patternsURI.getPath());
-                    String patternsFileName = patternsPath.getName().toString();
-                    parseSkipFile(patternsFileName);
-                }
-            }
-        }
-
-        private void parseSkipFile(String fileName) {
-            try {
-                fis = new BufferedReader(new FileReader(fileName));
-                String pattern = null;
-                while ((pattern = fis.readLine()) != null) {
-                    patternsToSkip.add(pattern);
-                }
-            } catch (IOException ioe) {
-                System.err.println("Caught exception while parsing the cached file '"
-                        + StringUtils.stringifyException(ioe));
-            }
-        }
-
-        @Override
-        public void map(Object key, Text value, Context context
-        ) throws IOException, InterruptedException {
-            String line = (caseSensitive) ? value.toString() : value.toString().toLowerCase();
-            for (String pattern : patternsToSkip) {
-                line = line.replaceAll(pattern, "");
-            }
-
-            String[] tuple = line.split("\\n");
-            try {
-                // For each document (JSON)
-                for (String str : tuple) {
-                    JSONObject obj = new JSONObject(str);
-                    IntWritable d_id = new IntWritable(obj.getInt("id"));
-                    String text = obj.getString("text");
-                    StringTokenizer itr = new StringTokenizer(text);
-                    while (itr.hasMoreTokens()) {
-                        whash.set(itr.nextToken().hashCode());
-                        context.write(d_id, whash);
-                    }
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public static class IntSumReducer extends Reducer<IntWritable, IntWritable, IntWritable, MapWritable> {
-        private final IntWritable ONE = new IntWritable(1);
-
-        public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-            MapWritable counter = new MapWritable();
-//            System.out.println("key: " + key.toString());
-            for (IntWritable val : values) {
-                System.out.println("value: " + val.toString());
-                if (counter.containsKey(val)) {
-                    counter.put(new IntWritable(val.get()), new IntWritable(((IntWritable) counter.get(val)).get() + 1));
-                } else {
-                    counter.put(new IntWritable(val.get()), ONE);
-                }
-            }
-//            System.out.println(Arrays.toString(counter.entrySet().toArray()));
-//            System.out.println("kek: " + counter.get(new IntWritable(-782211141)));
-            context.write(key, counter);
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
-        Configuration conf = new Configuration();
-        GenericOptionsParser optionParser = new GenericOptionsParser(conf, args);
-        String[] remainingArgs = optionParser.getRemainingArgs();
-        if ((remainingArgs.length != 2) && (remainingArgs.length != 4)) {
-            System.err.println("Usage: <command> <in> <out> [-skip skipPatternFile]");
-            System.exit(2);
-        }
-        Job job = Job.getInstance(conf, "search_engine");
-        job.setJarByClass(SearchEngine.class);
-        job.setMapperClass(TokenizerMapper.class);
-        job.setReducerClass(IntSumReducer.class);
-//        job.setCombinerClass(IntSumReducer.class);
-//        job.setOutputKeyClass(Text.class);
-//        job.setOutputValueClass(IntWritable.class);
-
-        job.setMapOutputKeyClass(IntWritable.class);
-        job.setMapOutputValueClass(IntWritable.class);
-        job.setOutputKeyClass(IntWritable.class);
-        job.setOutputValueClass(MapWritable.class);
-        job.setInputFormatClass(TextInputFormat.class);
-        job.setOutputFormatClass(MapFileOutputFormat.class);
-
-        List<String> otherArgs = new ArrayList<String>();
-        for (int i = 0; i < remainingArgs.length; ++i) {
-            if ("-skip".equals(remainingArgs[i])) {
-                job.addCacheFile(new Path(remainingArgs[++i]).toUri());
-                job.getConfiguration().setBoolean("search_engine.skip.patterns", true);
-            } else {
-                otherArgs.add(remainingArgs[i]);
-            }
-        }
-        FileInputFormat.addInputPath(job, new Path(otherArgs.get(0)));
-        File tmpDir = new File(args[1]);
-        boolean exists = tmpDir.exists();
-        if (exists) {
-            FileUtils.deleteDirectory(tmpDir);
-        }
-        FileOutputFormat.setOutputPath(job, new Path(otherArgs.get(1)));
-
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
-    }
+//
+//    public static class TokenizerMapper extends Mapper<Object, Text, IntWritable, IntWritable> {
+//
+//        private IntWritable whash = new IntWritable();
+//
+//        private boolean caseSensitive = true;
+//        private Set<String> patternsToSkip = new HashSet<String>();
+//
+//        private Configuration conf;
+//        private BufferedReader fis;
+//
+//        @Override
+//        public void setup(Context context) throws IOException {
+//            conf = context.getConfiguration();
+////            caseSensitive = conf.getBoolean("wordcount.case.sensitive", true);
+//            if (conf.getBoolean("wordcount.skip.patterns", false)) {
+//                URI[] patternsURIs = Job.getInstance(conf).getCacheFiles();
+//                for (URI patternsURI : patternsURIs) {
+//                    Path patternsPath = new Path(patternsURI.getPath());
+//                    String patternsFileName = patternsPath.getName().toString();
+//                    parseSkipFile(patternsFileName);
+//                }
+//            }
+//        }
+//
+//        private void parseSkipFile(String fileName) {
+//            try {
+//                fis = new BufferedReader(new FileReader(fileName));
+//                String pattern = null;
+//                while ((pattern = fis.readLine()) != null) {
+//                    patternsToSkip.add(pattern);
+//                }
+//            } catch (IOException ioe) {
+//                System.err.println("Caught exception while parsing the cached file '"
+//                        + StringUtils.stringifyException(ioe));
+//            }
+//        }
+//
+//        @Override
+//        public void map(Object key, Text value, Context context
+//        ) throws IOException, InterruptedException {
+//            String line = (caseSensitive) ? value.toString() : value.toString().toLowerCase();
+//            for (String pattern : patternsToSkip) {
+//                line = line.replaceAll(pattern, "");
+//            }
+//
+//            String[] tuple = line.split("\\n");
+//            try {
+//                // For each document (JSON)
+//                for (String str : tuple) {
+//                    JSONObject obj = new JSONObject(str);
+//                    IntWritable d_id = new IntWritable(obj.getInt("id"));
+//                    String text = obj.getString("text");
+//                    StringTokenizer itr = new StringTokenizer(text);
+//                    while (itr.hasMoreTokens()) {
+//                        whash.set(itr.nextToken().hashCode());
+//                        context.write(d_id, whash);
+//                    }
+//                }
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+//
+//    public static class IntSumReducer extends Reducer<IntWritable, IntWritable, IntWritable, MapWritable> {
+//        private final IntWritable ONE = new IntWritable(1);
+//
+//        public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+//            MapWritable counter = new MapWritable();
+//            for (IntWritable val : values) {
+//                System.out.println("value: " + val.toString());
+//                if (counter.containsKey(val)) {
+//                    counter.put(new IntWritable(val.get()), new IntWritable(((IntWritable) counter.get(val)).get() + 1));
+//                } else {
+//                    counter.put(new IntWritable(val.get()), ONE);
+//                }
+//            }
+//            context.write(key, counter);
+//        }
+//    }
+//
+//    public static void main(String[] args) throws Exception {
+//        Configuration conf = new Configuration();
+//        GenericOptionsParser optionParser = new GenericOptionsParser(conf, args);
+//        String[] remainingArgs = optionParser.getRemainingArgs();
+//        if ((remainingArgs.length != 2) && (remainingArgs.length != 4)) {
+//            System.err.println("Usage: <command> <in> <out> [-skip skipPatternFile]");
+//            System.exit(2);
+//        }
+//        Job job = Job.getInstance(conf, "search_engine");
+//        job.setJarByClass(SearchEngine.class);
+//        job.setMapperClass(TokenizerMapper.class);
+//        job.setReducerClass(IntSumReducer.class);
+//
+//        job.setMapOutputKeyClass(IntWritable.class);
+//        job.setMapOutputValueClass(IntWritable.class);
+//        job.setOutputKeyClass(IntWritable.class);
+//        job.setOutputValueClass(MapWritable.class);
+//        job.setInputFormatClass(TextInputFormat.class);
+//        job.setOutputFormatClass(SequenceFileOutputFormat.class);
+//
+//        List<String> otherArgs = new ArrayList<String>();
+//        for (int i = 0; i < remainingArgs.length; ++i) {
+//            if ("-skip".equals(remainingArgs[i])) {
+//                job.addCacheFile(new Path(remainingArgs[++i]).toUri());
+//                job.getConfiguration().setBoolean("search_engine.skip.patterns", true);
+//            } else {
+//                otherArgs.add(remainingArgs[i]);
+//            }
+//        }
+//        FileInputFormat.addInputPath(job, new Path(otherArgs.get(0)));
+//        File tmpDir = new File(args[1]);
+//        boolean exists = tmpDir.exists();
+//        if (exists) {
+//            FileUtils.deleteDirectory(tmpDir);
+//        }
+//        FileOutputFormat.setOutputPath(job, new Path(otherArgs.get(1)));
+//
+//        System.exit(job.waitForCompletion(true) ? 0 : 1);
+//    }
 }
